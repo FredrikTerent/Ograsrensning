64edff0831999eca23d451a703ef0f66d949aa52
Åsa Steinholtz <asa.steinholtz@yaelev.se>
2023-05-23 11:59:25 +0200

    Updated weedcontroller

---
+++ OgraasApi/Controllers/ClickDtoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OgraasApi.Data;
+using OgraasApi.Logic;
+using OgraasApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OgraasApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClickDtoController : ControllerBase
+    {
+        private readonly IPlayer playerRepo;
+        private GameLogic Logic;
+
+        public ClickDtoController(IPlayer playerRepo)
+        {
+            this.playerRepo = playerRepo;
+            Logic = new GameLogic();
+        }      
+
+        // POST api/<ClickDtoController>
+        [HttpPost("{id},{Row},{Col}")]
+        public HitResponse Post([FromBody] int id, int Row, int Col)
+        {
+
+            int x= Row, y = Col;
+            var player = playerRepo.GetByIdAsync(id).Result;
+            var hitResponse = 
+            Logic.Pangakrut(player, x, y);
+
+            return hitResponse;
+        }
+      
+    }
+}
--- OgraasApi/Controllers/WeedController.cs
+++ OgraasApi/Controllers/WeedController.cs
@@ -8,49 +8,132 @@ namespace OgraasApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-   
-        public class WeedController : ControllerBase
+
+    public class WeedController : ControllerBase
+    {
+        private readonly IWeed weedRepo;
+
+        public WeedController(IWeed weedRepo)
         {
-            private readonly IWeed weedRepo;
+            this.weedRepo = weedRepo;
+        }
+        // GET: api/<BoardController>
+        [HttpGet]
+        public async Task<IEnumerable<Maskros>> GetAllMaskros()
+        {
+            return await weedRepo.GetAllMaskrosAsync();
+        }
+        
+        [HttpGet]
+        public async Task<IEnumerable<Kardborre>> GetAllKardborre()
+        {
+            return await weedRepo.GetAllKardborreAsync();
+        }
+        
+        [HttpGet]
+        public async Task<IEnumerable<Kvickrot>> GetAllKvickrot()
+        {
+            return await weedRepo.GetAllKvickrotAsync();
+        }
+       
+        [HttpGet]
+        public async Task<IEnumerable<Kirskal>> GetAllKirskal()
+        {
+            return await weedRepo.GetAllKirskalAsync();
+        }
 
-            public WeedController(IWeed weedRepo)
-            {
-                this.weedRepo = weedRepo;
-            }
-            // GET: api/<BoardController>
-            [HttpGet]
-            public async Task<IEnumerable<Weed>> Get()
-            {
-                return await weedRepo.GetAllAsync();
-            }
+        // GET api/<BoardController>/5
+        [HttpGet("{id}")]
+        public async Task<Maskros> GetMaskros(int id)
+        {
+            return await weedRepo.GetMaskrosByIdAsync(id);
+        }
 
-            // GET api/<BoardController>/5
-            [HttpGet("{id}")]
-            public async Task<Weed> Get(int id)
-            {
-                return await weedRepo.GetByIdAsync(id);
-            }
+        [HttpGet("{id}")]
+        public async Task<Kardborre> GetKardborre(int id)
+        {
+            return await weedRepo.GetKardborreByIdAsync(id);
+        }
 
-            // POST api/<BoardController>
-            [HttpPost]
-            public async Task<Weed> Post([FromBody] Weed weed)
-            {
-                return await weedRepo.CreateAsync(weed);
-            }
+        [HttpGet("{id}")]
+        public async Task<Kvickrot> GetKvickrot(int id)
+        {
+            return await weedRepo.GetKvickrotByIdAsync(id);
+        }
 
-            // PUT api/<BoardController>/5
-            [HttpPut("{id}")]
-            public async Task Put(int id, [FromBody] Weed weed)
-            {
-                await weedRepo.UpdateAsync(weed);
-            }
+        [HttpGet("{id}")]
+        public async Task<Kirskal> GetKirskal(int id)
+        {
+            return await weedRepo.GetKirskalByIdAsync(id);
+        }
 
-            // DELETE api/<BoardController>/5
-            [HttpDelete("{id}")]
-            public async Task Delete(int id)
-            {
-                await weedRepo.DeleteAsync(id);
-            }
+        // POST api/<BoardController>
+        [HttpPost]
+        public async Task<Maskros> Post([FromBody] Maskros maskros)
+        {
+            return await weedRepo.CreateMaskrosAsync(maskros);
+        }
+        [HttpPost]
+        public async Task<Kardborre> Post([FromBody] Kardborre kardborre)
+        {
+            return await weedRepo.CreateKardborreAsync(kardborre);
+        }
+        [HttpPost]
+        public async Task<Kvickrot> Post([FromBody] Kvickrot kvickrot)
+        {
+            return await weedRepo.CreateKvickrotAsync(kvickrot);
+        }
+        [HttpPost]
+        public async Task<Kirskal> Post([FromBody] Kirskal kirskal)
+        {
+            return await weedRepo.CreateKirskalAsync(kirskal);
+        }
+
+        // PUT api/<BoardController>/5
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody] Maskros maskros)
+        {
+            await weedRepo.UpdateMaskrosAsync(maskros);
+        }
+        [HttpPut("{id}")]
+        public async Task PutKirskal(int id, [FromBody] Kirskal kirskal)
+        {
+            await weedRepo.UpdateKirskalAsync(kirskal);
+        }
+
+        [HttpPut("{id}")]
+        public async Task PutKvickrot(int id, [FromBody] Kvickrot kvickrot)
+        {
+            await weedRepo.UpdateKvickrotAsync(kvickrot);
+        }
+        [HttpPut("{id}")]
+        public async Task PutKardborre(int id, [FromBody] Kardborre kardborre)
+        {
+            await weedRepo.UpdateKardborreAsync(kardborre);
+        }
+
+
+        // DELETE api/<BoardController>/5
+        [HttpDelete("{id}")]
+        public async Task DeleteMaskrosAsync(int id)
+        {
+            await weedRepo.DeleteMaskrosAsync(id);
+        }
+        [HttpDelete("{id}")]
+        public async Task DeleteKardborreAsync(int id)
+        {
+            await weedRepo.DeleteKardborreAsync(id);
+        }
+        [HttpDelete("{id}")]
+        public async Task DeleteKvickrotAsync(int id)
+        {
+            await weedRepo.DeleteKvickrotAsync(id);
+        }
+        [HttpDelete("{id}")]
+        public async Task DeleteKirskalAsync(int id)
+        {
+            await weedRepo.DeleteKirskalAsync(id);
         }
     }
+}
 
